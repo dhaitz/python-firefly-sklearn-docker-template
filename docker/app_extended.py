4cbbe2a80e92ca6fd7200b2a5140cdd80a3f03c9
Dominik <dominik.haitz@1und1.de>
2018-12-21 15:14:53 +0100

    update extension

--- docker/app_extended.py
+++ docker/app_extended.py
@@ -4,7 +4,7 @@
 import pickle
 import logging
 
-log = logging.getLogger("prediction")
+log = logging.getLogger(f"[{__name__}]")
 
 model = pickle.load(open('model.pkl', 'rb'))
 
@@ -20,6 +20,7 @@ def predict(features):
 
     features_processed = feature_processing(features)
 
+    # apply model
     try:
         prediction = model.predict([features_processed])
         output = list(prediction)[0]
@@ -32,25 +33,35 @@ def predict(features):
     return output
 
 
-def is_valid_input(features):
+def is_valid_input(features) -> bool:
+    """Check if features can be casted to float"""
     for index, feature in enumerate(features):
         log.info(f"Validating feature {index}: {feature}")
 
         try:
             float(feature)
         except ValueError as value_error:
-            log.error(f"Could not convert to float {value_error}")
+            log.error(value_error)
             return False
 
+    log.info("Input successfully validated")
     return True
 
 
 def feature_processing(features):
-    # add some meta features
+    # ToDo: add meta feature generation if needed
     return features
 
 
-def is_valid_output(output):
-    log.info(output)
-    # do some checks
+def is_valid_output(output) -> bool:
+    """Check if output can be casted to float"""
+    log.info(f"Output validation: {output}")
+
+    try:
+        float(output)
+    except ValueError as value_error:
+        log.error(value_error)
+        return False
+
+    log.info("Output successfully validated")
     return True
